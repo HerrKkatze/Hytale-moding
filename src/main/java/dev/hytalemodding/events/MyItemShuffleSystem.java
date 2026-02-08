f522008b2854b78e80c6f4f673f3e60ba32cd40a
DadoIsSmArt <lundzeki@gmail.com>
2026-02-08 20:15:35 +0500

    off gravity

--- src/main/java/dev/hytalemodding/events/MyItemShuffleSystem.java
+++ src/main/java/dev/hytalemodding/events/MyItemShuffleSystem.java
@@ -5,6 +5,7 @@ import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.entity.item.ItemComponent;
+import com.hypixel.hytale.server.core.modules.physics.component.PhysicsValues;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import javax.annotation.Nonnull;
 
@@ -16,18 +17,31 @@ public class MyItemShuffleSystem extends EntityTickingSystem<EntityStore> {
 
         var Item = store.getComponent(reference, ItemComponent.getComponentType());
         var transform = archetypeChunk.getComponent(i, TransformComponent.getComponentType());
+        var rigidBody = store.getComponent(reference, PhysicsValues.getComponentType() );
 
-        assert transform != null;
 
-        var currentPosition = transform.getPosition();
-        currentPosition.add(0,0.2,0);
+        if (Item == null) return;
+        if (Item.getItemStack() == null) return;
+        if (rigidBody  == null) return;
+        if (transform == null) return;
+        if (!Item.getItemStack().getItemId().equals("Fish_Jellyfish_Cyan_Item")) return;
+
+
+        PhysicsValues values = new PhysicsValues(0,0,false);
+
+        rigidBody.replaceValues(values);
+
+        transform.getPosition().add(0,0.2,0);
+
+
     }
 
     @Override
     public Query<EntityStore> getQuery() {
          return Query.and(
                 ItemComponent.getComponentType(),
-                TransformComponent.getComponentType()
+                TransformComponent.getComponentType(),
+                 PhysicsValues.getComponentType()
         );
     }
 }
